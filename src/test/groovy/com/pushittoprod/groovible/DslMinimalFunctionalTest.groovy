abf136f2b9414cb3e5a5ff3a0e3a6ba8d3e4bfd9
pushittoprod <pushittoprod@gmail.com>
2019-05-19 13:49:13 -0700

    add a smaller FT for more targeted work

---
+++ src/test/groovy/com/pushittoprod/groovible/DslMinimalFunctionalTest.groovy
@@ -0,0 +1,40 @@
+package com.pushittoprod.groovible
+import org.junit.Test
+
+import static com.pushittoprod.groovible.AnsibleDsl.*
+
+
+class DslMinimalFunctionalTest {
+    def createPlaybook = {
+        playbook {
+            play {
+                hosts = "hosts"
+                vars {
+                    foo = 'bar'
+                }
+            }
+        }
+    }
+
+    def expected_yml = '''
+        ---
+        - hosts: "hosts"
+          vars:
+            foo: "bar"
+    '''.stripIndent().trim()
+
+    @Test void playbookCreatesAPlay() {
+        AnsiblePlaybook playbook = createPlaybook()
+        assert playbook.plays.size() == 1
+    }
+
+    @Test void playbookSetsHosts() {
+        AnsiblePlaybook playbook = createPlaybook()
+        assert playbook.plays[0].hosts == "hosts"
+    }
+
+    @Test void playbookSetsVars() {
+        AnsiblePlaybook playbook = createPlaybook()
+        assert playbook.plays[0].vars.foo == "bar"
+    }
+}
