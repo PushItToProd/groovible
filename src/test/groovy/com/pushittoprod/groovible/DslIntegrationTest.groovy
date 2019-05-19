61ef873bb7c4ed1acf8d222b4b02534765b3cb3a
pushittoprod <pushittoprod@gmail.com>
2019-05-19 14:16:13 -0700

    rename DslMinimalFunctionalTest to DslIntegrationTest since that's what it is

--- src/test/groovy/com/pushittoprod/groovible/DslIntegrationTest.groovy
+++ src/test/groovy/com/pushittoprod/groovible/DslIntegrationTest.groovy
@@ -4,7 +4,7 @@ import org.junit.Test
 import static com.pushittoprod.groovible.AnsibleDsl.*
 
 
-class DslMinimalFunctionalTest {
+class DslIntegrationTest {
     def createPlaybook = {
         playbook {
             play {
