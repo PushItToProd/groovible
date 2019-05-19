492bb6193338a36f2928328e66b19e798f5edf1f
pushittoprod <pushittoprod@gmail.com>
2019-05-19 16:20:55 -0700

    add FT to capture case where notify is being included even when empty

--- src/test/groovy/com/pushittoprod/groovible/DslFunctionalTestNotify.groovy
+++ src/test/groovy/com/pushittoprod/groovible/DslFunctionalTestNotify.groovy
@@ -34,3 +34,30 @@ class DslFunctionalTestNotify {
         assert expected_output == output
     }
 }
+
+
+class DslFunctionalTestNotifyEmpty {
+    def input = playbook {
+        play {
+            tasks {
+                template {
+                    src = "/srv/httpd.j2"
+                    dest = "/etc/httpd.conf"
+                }
+            }
+        }
+    }
+
+    def expected_output = '''
+    ---
+    - tasks:
+      - template:
+          src: /srv/httpd.j2
+          dest: /etc/httpd.conf
+    '''.stripIndent().trim()
+
+    @Test void testExample() {
+        def output = input.compile().stripIndent().trim()
+        assert expected_output == output
+    }
+}
