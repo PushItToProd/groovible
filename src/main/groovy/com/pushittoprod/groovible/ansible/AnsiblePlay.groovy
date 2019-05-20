ff08b5865c0ab6c41e34870cd30917ec160a727b
pushittoprod <pushittoprod@gmail.com>
2019-05-19 19:32:16 -0700

    fix formatting so that functional tests pass again

--- src/main/groovy/com/pushittoprod/groovible/ansible/AnsiblePlay.groovy
+++ src/main/groovy/com/pushittoprod/groovible/ansible/AnsiblePlay.groovy
@@ -1,8 +1,10 @@
 package com.pushittoprod.groovible.ansible
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import groovy.transform.Canonical
 
 @Canonical
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 class AnsiblePlay {
     String name = null
     String hosts = null
