d08d2989a843fe6b700b15fea55639d8f583402b
pushittoprod <pushittoprod@gmail.com>
2019-05-21 20:06:35 -0700

    remove need for outer playbook block in script files

--- src/main/groovy/com/pushittoprod/groovible/compiler/DslCompiler.groovy
+++ src/main/groovy/com/pushittoprod/groovible/compiler/DslCompiler.groovy
@@ -1,22 +1,38 @@
 package com.pushittoprod.groovible.compiler
 
+import com.pushittoprod.groovible.ansible.AnsiblePlay
 import com.pushittoprod.groovible.ansible.AnsiblePlaybook
-import com.pushittoprod.groovible.dsl.AnsiblePlaybookBuilder
+import com.pushittoprod.groovible.dsl.AnsiblePlayBuilder
 import com.pushittoprod.groovible.serialization.Serialization
 
 class DslCompiler {
     DslCompiler() {
     }
 
+    def play = { playbook, name_, cl_ = null ->
+        assert name_ != null
+        String name = null
+        Closure cl
+        if (cl_ != null) {
+            name = name_ as String
+            cl = cl_ as Closure
+        } else {
+            cl = name_ as Closure
+        }
+        assert cl != null, "play requires a non-null closure argument"
+        def play = new AnsiblePlay(name)
+        new AnsiblePlayBuilder(play).build(cl)
+        playbook.add(play)
+    }
+
     AnsiblePlaybook compileToPlaybook(String groovy) {
         def binding = new Binding()
         def shell = new GroovyShell(binding)
-        def playbookCl = { Closure cl ->
-            AnsiblePlaybookBuilder.playbook(cl)
-        }
-        binding.setProperty('playbook', playbookCl)
-        AnsiblePlaybook result = shell.evaluate(groovy) as AnsiblePlaybook
-        return result
+        AnsiblePlaybook playbook = new AnsiblePlaybook()
+        def play = play.curry(playbook)
+        binding.setProperty('play', play)
+        shell.evaluate(groovy)
+        return playbook
     }
 
     String compile(String groovy) {
--- src/test/groovy/com/pushittoprod/groovible/compiler/DslCompilerTest.groovy
+++ src/test/groovy/com/pushittoprod/groovible/compiler/DslCompilerTest.groovy
@@ -4,40 +4,38 @@ import org.junit.Test
 
 class DslCompilerTest {
     def inputScript = '''
-        playbook {
-            // a play with no name
-            play {
-                hosts = "webservers"
-                vars {
-                    http_port = 80
-                    max_clients = 200
-                }
-                remote_user = "root"
+        // a play with no name
+        play {
+            hosts = "webservers"
+            vars {
+                http_port = 80
+                max_clients = 200
+            }
+            remote_user = "root"
 
-                tasks {
-                    yum("ensure apache is at the latest version") {
-                        name = "httpd"
-                        state = "latest"
-                    }
-
-                    template("write the apache config file") {
-                        src = "/srv/httpd.j2"
-                        dest = "/etc/httpd.conf"
-
-                        notify "restart apache"
-                    }
-
-                    service("ensure apache is running") {
-                        name = "httpd"
-                        state = "started"
-                    }
+            tasks {
+                yum("ensure apache is at the latest version") {
+                    name = "httpd"
+                    state = "latest"
                 }
 
-                handlers {
-                    service("restart apache") {
-                        name = "httpd"
-                        state = "restarted"
-                    }
+                template("write the apache config file") {
+                    src = "/srv/httpd.j2"
+                    dest = "/etc/httpd.conf"
+
+                    notify "restart apache"
+                }
+
+                service("ensure apache is running") {
+                    name = "httpd"
+                    state = "started"
+                }
+            }
+
+            handlers {
+                service("restart apache") {
+                    name = "httpd"
+                    state = "restarted"
                 }
             }
         }
